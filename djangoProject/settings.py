81e5b6867b4a783444052bdf6dfc3b1d454d346e
Innocent Kiwoly <ikiwoly@gmail.com>
2024-12-06 23:07:34 +0300

    changes
    on setting

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')]
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').strip('[]').replace('"', '').split(',')
 
 
 
